aec2f7ee8fdf97f6614213442c13e5925b12dbaa
Sultan Mohammad <mohaimin.bonny@gmail.com>
2020-03-30 22:02:33 +0600

    changed in caption

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React, creating a tic tac toe
         </a>
       </header>
     </div>
